0620a63b6a90c56dafdd605dc256c7c04f8a0f60
Wirecats5686 <frc5686@my.ethelwalker.org>
2019-02-14 20:52:26 -0800

    Low Shoot trigger fix missed in other commit

--- Wirecats 2019 Actual/src/main/java/frc/robot/triggers/LowShootTrigger.java
+++ Wirecats 2019 Actual/src/main/java/frc/robot/triggers/LowShootTrigger.java
@@ -1,21 +1,25 @@
 package frc.robot.triggers;
 
-import frc.robot.Robot;
 import frc.robot.GamepadTriggerPOVMap;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 /**
- * This is the trigger to shoot the cargo into a Cargo Ship slot
+ * This is the trigger to shoot the cargo into a rocket slot
  */
 public class LowShootTrigger extends Trigger {
+
+    private Joystick gamepad;
+
+    public LowShootTrigger (Joystick joystick) {
+        gamepad = joystick;
+    }
     
     /**
      * Determine if trigger is being pressed
      * getRawAxis(3) represents right trigger on gamepad
      * @return true if trigger is activated, else false
-     * TODO: Determine and experiment with return values of getRawAxis(); should be between -1 and 1
      */
     public boolean get() {
-        return true;
-        // return Robot.oi.getGamepad().getRawAxis(GamepadTriggerPOVMap.rightTrigger) != 0;
+        return gamepad.getRawAxis(GamepadTriggerPOVMap.rightTrigger) != 0;
     }
 }
